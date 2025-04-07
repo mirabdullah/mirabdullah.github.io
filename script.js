ae3c447dfeab73142ad2576bd4c816c29c71578f
mirabdullah <52750083+mirabdullah@users.noreply.github.com>
2025-04-07 16:32:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // Add any interactivity here if needed
 console.log("Bio website loaded!");
+function openMenu() {
+  document.getElementById("sideMenu").style.left = "0";
+  document.getElementById("mainContent").style.marginLeft = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").style.left = "-250px";
+  document.getElementById("mainContent").style.marginLeft = "0";
+}
