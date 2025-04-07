c5142bbc241e814b93efacddc1d63f966d52cfcf
mirabdullah <52750083+mirabdullah@users.noreply.github.com>
2025-04-07 16:38:21 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 // Add any interactivity here if needed
-console.log("Bio website loaded!");
-function openMenu() {
-  document.getElementById("sideMenu").style.left = "0";
-  document.getElementById("mainContent").style.marginLeft = "250px";
-}
-
-function closeMenu() {
-  document.getElementById("sideMenu").style.left = "-250px";
-  document.getElementById("mainContent").style.marginLeft = "0";
-}
+console.log("Personal bio website loaded!");
