0969b8a6c2e6fc515e36f9d829ce7c9a514f68ab
mirabdullah <52750083+mirabdullah@users.noreply.github.com>
2025-04-26 20:04:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-// Add any interactivity here if needed
-console.log("Personal bio website loaded!");
+// Simple pop-up when clicking the "Contact Me" button
+document.querySelector('.btn').addEventListener('click', function() {
+  alert('Thanks for reaching out!');
+});
